1334b34bf676f51a916bad27a600a969cdd4f77e
Stefan Dahms <std@stdx.de>
2024-03-20 21:22:17 +0100

    Remove obsolet method (same behaviour as super)

--- app/src/main/java/com/git/amarradi/leafpad/NoteEditActivity.java
+++ app/src/main/java/com/git/amarradi/leafpad/NoteEditActivity.java
@@ -170,7 +170,6 @@ public class NoteEditActivity extends AppCompatActivity {
         sendIntent.putExtra(Intent.EXTRA_TEXT, getExportString());
         sendIntent.setType("text/plain");
         startActivity(Intent.createChooser(sendIntent, getString(R.string.share_note)));
-
     }
 
     public String getExportString() {
@@ -186,8 +185,4 @@ public class NoteEditActivity extends AppCompatActivity {
         return exportString;
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-    }
 }
