45f380402e9ab9cf2e0216c15305ce01de7103fa
marcus <radischm@gmail.com>
2025-02-12 11:56:32 +0100

    solved #122

--- app/src/main/java/com/git/amarradi/leafpad/SettingsActivity.java
+++ app/src/main/java/com/git/amarradi/leafpad/SettingsActivity.java
@@ -1,22 +1,33 @@
 package com.git.amarradi.leafpad;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.appbar.MaterialToolbar;
-
-import java.util.Objects;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         setTheme(R.style.Theme_App_toolbar);
+
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
     }
 }
