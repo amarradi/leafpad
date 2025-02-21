49c61aa2a0a05226c4724c75a011e4a213959f85
marcus <radischm@gmail.com>
2025-02-21 09:42:49 +0100

    New style added

--- app/src/main/java/com/git/amarradi/leafpad/SettingsActivity.java
+++ app/src/main/java/com/git/amarradi/leafpad/SettingsActivity.java
@@ -1,33 +1,42 @@
 package com.git.amarradi.leafpad;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
+import java.util.Objects;
+
 public class SettingsActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        setTheme(R.style.Theme_App_toolbar);
-
-        EdgeToEdge.enable(this);
+        setTheme(R.style.Theme_MyApp);
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.activity_settings);
+
+        MaterialToolbar toolbar = findViewById(R.id.setting_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+       // EdgeToEdge.enable(this);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.settings_fragment_container, new SettingsFragment())
+                    .commit();
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.setting_toolbar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(0, systemBars.top, 0, 0);
             return insets;
         });
-
     }
 }
