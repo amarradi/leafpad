089d7798cc17ee6d6a6e71e87dee9c48e0379bad
marcus <radischm@gmail.com>
2025-11-05 16:16:09 +0100

    about Activity

--- app/src/main/java/com/git/amarradi/leafpad/AboutActivity.java
+++ app/src/main/java/com/git/amarradi/leafpad/AboutActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.git.amarradi.leafpad.helper.TranslationThanksBuilder;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -20,18 +24,31 @@ public class AboutActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_MyApp);
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
         setContentView(R.layout.activity_about);
         MaterialToolbar materialToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(materialToolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbar), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, systemBars.top, 0, 0);
+            return insets;
+        });
         MaterialTextView materialTextViewAppNameVersion = findViewById(R.id.textView_version);
         MaterialTextView materialTextViewStatement = findViewById(R.id.textView_statement);
         materialTextViewAppNameVersion.setText(getApplicationName(getBaseContext()) + " " + getApplicationNumber());
         materialTextViewStatement.setText(R.string.statement);
         MaterialTextView weblateView = findViewById(R.id.thank_you_for_weblate);
         weblateView.setText(TranslationThanksBuilder.build(this));
+
+
     }
 
     public String getApplicationNumber() {
