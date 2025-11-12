ad46e04eefde6b40da0c3b53b09a549b080560af
marcus <radischm@gmail.com>
2025-11-12 13:16:38 +0100

    Refactor: Separate About from Settings and update translations
    - Moved the "About" section from Preferences to its own dedicated menu item and activity.
    - Updated and shortened several UI strings for clarity.
    - Updated release notes.
    - Incorporated new and updated translations for multiple languages including Danish, Dutch, Gothic, and others.

--- app/src/main/java/com/git/amarradi/leafpad/helper/TranslationThanksBuilder.java
+++ app/src/main/java/com/git/amarradi/leafpad/helper/TranslationThanksBuilder.java
@@ -16,69 +16,55 @@ public class TranslationThanksBuilder {
 
         appendBoldCountry(builder, context.getString(R.string.country_title_albanian));
         builder.append(context.getString(R.string.country_title_translator_albanian)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_arabic));
         builder.append(context.getString(R.string.country_title_translator_arabic)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_catalan));
         builder.append(context.getString(R.string.country_title_translator_catalan)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_chinese_simplified_han_script));
         builder.append(context.getString(R.string.country_title_translator_chinese_simplified_han_script)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_chinese_traditional_han_script));
-        builder.append(context.getString(R.string.country_title_translator_chinese_simplified_han_script)).append("\n");
-
+        builder.append(context.getString(R.string.country_title_translator_chinese_traditional_han_script)).append("\n");
         appendBoldCountry(builder, context.getString(R.string.country_title_czech));
         builder.append(context.getString(R.string.country_title_translator_czech)).append("\n");
-
+        appendBoldCountry(builder, context.getString(R.string.country_title_danish));
+        builder.append(context.getString(R.string.country_title_translator_danish)).append("\n");
+        appendBoldCountry(builder, context.getString(R.string.country_title_dutch));
+        builder.append(context.getString(R.string.country_title_translator_dutch)).append("\n");
         appendBoldCountry(builder, context.getString(R.string.country_title_english));
         builder.append(context.getString(R.string.country_title_translator_english)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_estonian));
         builder.append(context.getString(R.string.country_title_translator_estonian)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_french));
         builder.append(context.getString(R.string.country_title_translator_french)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_german));
         builder.append(context.getString(R.string.country_title_translator_german)).append("\n");
-
+        appendBoldCountry(builder, context.getString(R.string.country_title_gothic));
+        builder.append(context.getString(R.string.country_title_translator_gothic)).append("\n");
         appendBoldCountry(builder, context.getString(R.string.country_title_greek));
         builder.append(context.getString(R.string.country_title_translator_greek)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_hindi));
         builder.append(context.getString(R.string.country_title_translator_hindi)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_hindi_latin_script));
         builder.append(context.getString(R.string.country_title_translator_hindi_latin_script)).append("\n");
-
-        appendBoldCountry(builder, context.getString(R.string.country_title_hindi));
-        builder.append(context.getString(R.string.country_title_translator_hindi)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_indonesian));
         builder.append(context.getString(R.string.country_title_translator_indonesian)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_italian));
         builder.append(context.getString(R.string.country_title_translator_italian)).append("\n");
-
+        appendBoldCountry(builder, context.getString(R.string.country_title_polish));
+        builder.append(context.getString(R.string.country_title_translator_polish)).append("\n");
         appendBoldCountry(builder, context.getString(R.string.country_title_portuguese));
         builder.append(context.getString(R.string.country_title_translator_portuguese)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_russian));
         builder.append(context.getString(R.string.country_title_translator_russian)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_spanish));
         builder.append(context.getString(R.string.country_title_translator_spanish)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_tamil));
         builder.append(context.getString(R.string.country_title_translator_tamil)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_ukrainian));
         builder.append(context.getString(R.string.country_title_translator_ukrainian)).append("\n");
-
         appendBoldCountry(builder, context.getString(R.string.country_title_uzbek));
         builder.append(context.getString(R.string.country_title_translator_uzbek));
+
         return builder;
     }
 
